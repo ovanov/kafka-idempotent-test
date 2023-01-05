9e5ff211f6cf6dbc3e5462b0c75956e36b465718
ovanov <grepyugo@protonmail.com>
2023-01-05 15:17:50 +0100

    fixed authorization issue with schema registy

--- src/main/java/com/ipt/kafkatopicupdates/producer/AuthorizationAvroProducer.java
+++ src/main/java/com/ipt/kafkatopicupdates/producer/AuthorizationAvroProducer.java
@@ -1,6 +1,7 @@
 package com.ipt.kafkatopicupdates.producer;
 
 import ch.ipt.kafka.avro.Authorization;
+import com.google.rpc.context.AttributeContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
