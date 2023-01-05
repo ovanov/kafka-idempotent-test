c3bc57ec872ae4facd6aaa03986995ac14589fa0
Mirko <mirko.serbak@ipt.ch>
2023-01-05 16:16:07 +0100

    added consumer

--- src/main/java/com/ipt/kafkatopicupdates/consumer/KafkaConsumer.java
+++ src/main/java/com/ipt/kafkatopicupdates/consumer/KafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.ipt.kafkatopicupdates.consumer;
 
 
+import ch.ipt.kafka.avro.Authorization;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,32 +10,15 @@ import org.springframework.stereotype.Component;
 
 
 //This class is only needed when we want to consume messages directly
-//@Component
+@Component
 public class KafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
-
-    /*@KafkaListener(id = "transactions-consumer",
-            topics = "transactions")
-    public void receiveTransactionMessage(ConsumerRecord<String, Payment> consumerRecord) {
+    @KafkaListener(id = "authorization-consumer",
+            topics = "authorization")
+    public void receiveNewState(ConsumerRecord<String, Authorization> consumerRecord) {
         String key = consumerRecord.key();
-        Payment value = consumerRecord.value();
-        LOGGER.info("received credit message: key={}, value={}", key, value);
-    }*/
-
-   /* @KafkaListener(id = "accounts-consumer",
-            topics = "compacted")
-    public void receiveAccountMessage(ConsumerRecord<String, String> consumerRecord) {
-        String key = consumerRecord.key();
-        String value = consumerRecord.value();
-        LOGGER.info("received account message: key={}, value={}", key, value);
-    }
-
-    @KafkaListener(id = "state-transfer-consumer",
-            topics = "state-transfer")
-    public void receiveNewState(ConsumerRecord<String, String> consumerRecord) {
-        String key = consumerRecord.key();
-        String value = consumerRecord.value();
+        Authorization value = consumerRecord.value();
         LOGGER.info("received NEW 3DS STATE: key={}, value={}", key, value);
-    }*/
+    }
 }
